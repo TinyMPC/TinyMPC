40342021212d340781812e5962dacfd6b3427357
Sam Schoedel <seschoedel@gmail.com>
2024-05-07 14:29:08 -0400

    Clean up examples

--- examples/quadrotor_tracking.cpp
+++ examples/quadrotor_tracking.cpp
@@ -29,85 +29,75 @@
 extern "C"
 {
 
-    TinySolution *solution = new TinySolution();
-    TinyCache *cache = new TinyCache();
-    TinyWorkspace *work = new TinyWorkspace();
-    TinySettings *settings = new TinySettings();
-    TinySolver solver{solution, settings, cache, work};
+TinySolution solution;
+TinyCache cache;
+TinyWorkspace work;
+TinySettings settings;
+TinySolver solver{&solution, &settings, &cache, &work};
 
-    typedef Matrix<tinytype, NINPUTS, NHORIZON-1> tiny_MatrixNuNhm1;
-    typedef Matrix<tinytype, NSTATES, NHORIZON> tiny_MatrixNxNh;
-    typedef Matrix<tinytype, NSTATES, 1> tiny_VectorNx;
+typedef Matrix<tinytype, NINPUTS, NHORIZON-1> tiny_MatrixNuNhm1;
+typedef Matrix<tinytype, NSTATES, NHORIZON> tiny_MatrixNxNh;
+typedef Matrix<tinytype, NSTATES, 1> tiny_VectorNx;
 
-    int main()
+int main()
+{
+    work.Adyn = Map<Matrix<tinytype, NSTATES, NSTATES, RowMajor>>(Adyn_data);
+    work.Bdyn = Map<Matrix<tinytype, NSTATES, NINPUTS, RowMajor>>(Bdyn_data);
+    work.Q = Map<Matrix<tinytype, NSTATES, 1>>(Q_data);
+    work.R = Map<Matrix<tinytype, NINPUTS, 1>>(R_data);
+    
+    work.x_min = tiny_MatrixNxNh::Constant(-5);
+    work.x_max = tiny_MatrixNxNh::Constant(5);
+    work.u_min = tiny_MatrixNuNhm1::Constant(-0.5);
+    work.u_max = tiny_MatrixNuNhm1::Constant(0.5);
+
+    tiny_set_default_settings(&settings);
+
+    int status = tiny_setup(&cache, &work, &solution,
+                            work.Adyn, work.Bdyn, work.Q.asDiagonal(), work.R.asDiagonal(),
+                            rho_value, NSTATES, NINPUTS, NHORIZON,
+                            work.x_min, work.x_max, work.u_min, work.u_max,
+                            &settings, 1);
+
+    tiny_VectorNx x0, x1; // current and next simulation states
+
+    // Map data from trajectory_data
+    Matrix<tinytype, NSTATES, NTOTAL> Xref_total = Eigen::Map<Matrix<tinytype, NSTATES, NTOTAL>>(Xref_data);
+    work.Xref = Xref_total.block<NSTATES, NHORIZON>(0, 0);
+
+    // Initial state
+    x0 = work.Xref.col(0);
+
+    // std::cout << work.Xref << std::endl;
+
+    for (int k = 0; k < NTOTAL - NHORIZON; ++k)
     {
-        work->Adyn = Map<Matrix<tinytype, NSTATES, NSTATES, RowMajor>>(Adyn_data);
-        work->Bdyn = Map<Matrix<tinytype, NSTATES, NINPUTS, RowMajor>>(Bdyn_data);
-        work->Q = Map<Matrix<tinytype, NSTATES, 1>>(Q_data);
-        work->R = Map<Matrix<tinytype, NINPUTS, 1>>(R_data);
-        
-        work->x_min = tiny_MatrixNxNh::Constant(-5);
-        work->x_max = tiny_MatrixNxNh::Constant(5);
-        work->u_min = tiny_MatrixNuNhm1::Constant(-0.5);
-        work->u_max = tiny_MatrixNuNhm1::Constant(0.5);
+        std::cout << "tracking error: " << (x0 - work.Xref.col(1)).norm() << std::endl;
 
-        settings->abs_pri_tol = 0.001;
-        settings->abs_dua_tol = 0.001;
-        settings->max_iter = 100;
-        settings->check_termination = 1;
-        settings->en_input_bound = 1;
-        settings->en_state_bound = 1;
+        // 1. Update measurement
+        work.x.col(0) = x0;
 
-        std::cout << work->Q.asDiagonal() << std::endl;
+        // 2. Update reference
+        work.Xref = Xref_total.block<NSTATES, NHORIZON>(0, k);
 
-        int status = tiny_setup(cache, work, solution,
-                                work->Adyn, work->Bdyn, work->Q.asDiagonal(), work->R.asDiagonal(),
-                                rho_value, NSTATES, NINPUTS, NHORIZON,
-                                work->x_min, work->x_max, work->u_min, work->u_max,
-                                settings, 1);
+        // 3. Reset dual variables if needed
+        work.y = tiny_MatrixNuNhm1::Zero();
+        work.g = tiny_MatrixNxNh::Zero();
 
 
+        // 4. Solve MPC problem
+        tiny_solve(&solver);
 
-        tiny_VectorNx x0, x1; // current and next simulation states
+        // std::cout << work.iter << std::endl;
+        // std::cout << work.u.col(0).transpose().format(TinyFmt) << std::endl;
 
-        // Map data from trajectory_data
-        Matrix<tinytype, NSTATES, NTOTAL> Xref_total = Eigen::Map<Matrix<tinytype, NSTATES, NTOTAL>>(Xref_data);
-        work->Xref = Xref_total.block<NSTATES, NHORIZON>(0, 0);
+        // 5. Simulate forward
+        x0 = work.Adyn * x0 + work.Bdyn * work.u.col(0);
 
-        // Initial state
-        x0 = work->Xref.col(0);
-
-        // std::cout << work.Xref << std::endl;
-
-        for (int k = 0; k < NTOTAL - NHORIZON; ++k)
-        {
-            std::cout << "tracking error: " << (x0 - work->Xref.col(1)).norm() << std::endl;
-
-            // 1. Update measurement
-            work->x.col(0) = x0;
-
-            // 2. Update reference
-            work->Xref = Xref_total.block<NSTATES, NHORIZON>(0, k);
-
-            // 3. Reset dual variables if needed
-            work->y = tiny_MatrixNuNhm1::Zero();
-            work->g = tiny_MatrixNxNh::Zero();
-
-
-            // 4. Solve MPC problem
-            tiny_solve(&solver);
-
-            // std::cout << work.iter << std::endl;
-            // std::cout << work.u.col(0).transpose().format(TinyFmt) << std::endl;
-
-            // 5. Simulate forward
-            x1 = work->Adyn * x0 + work->Bdyn * work->u.col(0);
-            x0 = x1;
-
-            // std::cout << x0.transpose().format(TinyFmt) << std::endl;
-        }
-
-        return 0;
+        // std::cout << x0.transpose().format(TinyFmt) << std::endl;
     }
+
+    return 0;
+}
 
 } /* extern "C" */
