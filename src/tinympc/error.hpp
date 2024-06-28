7e1d133a4a0c63533d1f98ecea58deedd5aa4752
Sam Schoedel <seschoedel@gmail.com>
2024-06-28 13:02:20 -0400

    Use errno.h for all error messages

--- src/tinympc/error.hpp
+++ src/tinympc/error.hpp
@@ -1,21 +1,29 @@
 #pragma once
 
-#include <cstdio>   
-#include <cstdlib>  
-#include <cstring> 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-#if defined(__linux__) || defined(__unix__)// Check if Linux
-#include <error.h>
-#define ERROR_MSG(exit_code, format, ...) error(exit_code, errno, format, ##__VA_ARGS__)
-
-#elif defined(__APPLE__) || defined(__MACH__) // Check if macOS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <errno.h>
+
+// #if defined(__linux__) || defined(__unix__)// Check if Linux
+// #include <error.h>
+// #define ERROR_MSG(exit_code, format, ...) error(exit_code, errno, format, ##__VA_ARGS__)
+
+// #elif defined(__APPLE__) || defined(__MACH__) // Check if macOS
 #define ERROR_MSG(exit_code, format, ...) \
-    do { \
+        { \
         fprintf(stderr, format ": %s\n", ##__VA_ARGS__, strerror(errno)); \
         exit(exit_code); \
-    } while (0)
+        }
 
-#else
-#error "Unsupported operating system"
+// #else
+// #error "Unsupported operating system"
+// #endif
+
+#ifdef __cplusplus
+}
 #endif
