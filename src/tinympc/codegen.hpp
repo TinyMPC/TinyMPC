b597ca678d270bfed357e3118626791a71ec6730
Ishaan Mahajan <ishaanamahajan@gmail.com>
2025-04-09 15:36:34 -0400

    orig codegen

--- src/tinympc/codegen.hpp
+++ src/tinympc/codegen.hpp
@@ -7,14 +7,6 @@ extern "C" {
 #endif
 
     int tiny_codegen(TinySolver* solver, const char* output_dir, int verbose);
-
-    int tiny_codegen_with_sensitivity(TinySolver* solver, 
-                    const char* output_dir,
-                    const tinyMatrix* dK,
-                    const tinyMatrix* dP,
-                    const tinyMatrix* dC1,
-                    const tinyMatrix* dC2,
-                    int verbose);
     
     int codegen_create_directories(const char* output_dir, int verbose);
     int codegen_data_header(const char* output_dir, int verbose);
